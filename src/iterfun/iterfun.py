eec3989523818245faef16e390b13befdb3560d4
StefanGreve <greve.stefan@outlook.jp>
2021-12-20 21:44:23 +0100

    Implements a series of new functions.
    Note: Unchecked functions currently raise an NotImplementedError.
    
    - [x] overload __init__ with tuple range initialization
    - [x] implement count_until
    - [x] implement dedup
    - [ ] add dedup_by method signature
    - [x] implement drop
    - [x] implement drop_every
    - [x] implement drop_while
    - [x] implement each
    - [x] implement empty
    - [ ] add fetch method signature
    - [x] implement filter
    - [x] implement find
    - [x] implement find_index
    - [x] implement find_value
    - [x] implement flat_map
    - [ ] add flat_map_reduce method signature

--- src/iterfun/iterfun.py
+++ src/iterfun/iterfun.py
@@ -13,23 +13,36 @@ from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Tuple, Un
 
 class Iter:
     """
-    Provides a set of algorithms to work with iterable collections. In Python,
+    Define a set of algorithms to work with iterable collections. In Python,
     an iterable is any data type that implements the `__iter__` method which
     returns an iterator, or alternatively, a `__getitem__` method suitable for
     indexed lookup such as `list`, `tuple`, `dict`, or `set`.
 
     ```python
-    >>> Iter(range(1,4)).map(lambda x: 2*x)
+    >>> Iter(1, 3).map(lambda x: 2*x)
     [2, 4, 6]
-    >>> Iter(range(1, 4)).sum()
+    >>> Iter([1, 2, 3]).sum()
     6
-    >>> Iter({'a': 1, 'b': 2}).map(lambda k,v: {k, 2 * v})
+    >>> Iter({'a': 1, 'b': 2}).map(lambda k,v: {k, 2 * v}
     {'a': 2, 'b': 4}
     ```
     """
-    def __init__(self, iter: Iterable) -> Iter:
-        self.domain = list(iter)
-        self.image = iter
+
+    @overload
+    def __init__(self, iter: Iterable) -> Iter: ...
+
+    @overload
+    def __init__(self, *iter: int) -> Iter: ...
+
+    def __init__(self, *iter: Iterable | int) -> Iter:
+        self.domain = iter[0]
+        self.image = Iter.__ctor(iter)
+
+    @staticmethod
+    def __ctor(iter: Iterable | int) -> List:
+        if isinstance(iter, Tuple) and len(iter) == 2:
+            return list(range(iter[0], iter[1]+1)) if iter[1] != 0 else []
+        return iter[0]
 
     def all(self, fun: Optional[Callable]=None) -> bool:
         """
@@ -161,11 +174,205 @@ class Iter:
         ```python
         >>> Iter(range(1,4)).count()
         3
-        >>> Iter(range(1, 6).count(lambda x: x % 2 == 0))
+        >>> Iter(range(1, 6)).count(lambda x: x % 2 == 0)
         2
         ```
         """
-        return len(self.image) if fun is None else len(list(filter(fun, self.image)))
+        return len(list(self.image)) if fun is None else len(list(filter(fun, self.image)))
+
+    def count_until(self, limit: int, fun: Optional[Callable]=None) -> int:
+        """
+        Count the elements in `self.image` for which `fun` returns a truthy value,
+        stopping at `limit`.
+
+        ```python
+        >>> Iter(range(1, 21)).count_until(5)
+        5
+        >>> Iter(range(1, 21)).count_until(50)
+        20
+        ```
+        """
+        return len(list(self.image)[:limit]) if fun is None else len(list(filter(fun, self.image))[:limit])
+
+
+    def dedup(self) -> Iter:
+        """
+        Enumerates `self.image`, returning a list where all consecutive duplicated
+        elements are collapsed to a single element.
+
+        ```python
+        >>> Iter([1, 2, 3, 3, 2, 1]).dedup()
+        [1, 2, 3, 2, 1]
+        ```
+        """
+        self.image = [group[0] for group in itertools.groupby(self.image)]
+        return self
+
+    def dedup_by(self, fun: Callable):
+        raise NotImplementedError()
+
+    def drop(self, amount: int) -> Iter:
+        """
+        ```python
+        >>> Iter([1, 2, 3]).drop(2)
+        [3]
+        >>> Iter([1, 2, 3]).drop(10)
+        []
+        >>> Iter([1, 2, 3]).drop(-1)
+        [1, 2]
+        ```
+        """
+        self.image = list(self.image)
+        self.image = self.image[amount:] if amount > 0 else self.image[:len(self.image)+amount]
+        return self
+
+    def drop_every(self, nth: int) -> Iter:
+        """
+        Return a list of every `nth` element in the `self.image` dropped, starting
+        with the first element. The first element is always dropped, unless `nth` is `0`.
+        The second argument specifying every nth element must be a non-negative integer.
+
+        ```python
+        >>> Iter(1, 10).drop_every(2)
+        [2, 4, 6, 8, 10]
+        >>> Iter(1, 10).drop_every(0)
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        >>> Iter(1, 3).drop_every(1)
+        []
+        ```
+        """
+        self.image = [] if nth == 1 else [self.image[i] for i in range(int(nth != 0), len(self.image), nth if nth > 1 else 1)]
+        return self
+
+    def drop_while(self, fun: Callable) -> Iter:
+        """
+        Drop elements at the beginning of the enumerable while `fun` returns a
+        truthy value.
+
+        ```python
+        >>> Iter([1, 2, 3, 2, 1]).drop_while(lambda x: x < 3)
+        [3, 2, 1]
+        ```
+        """
+        self.image = self.image[self.image.index(list(itertools.filterfalse(fun, self.image))[0]):]
+        return self
+
+    def each(self, fun: Callable) -> bool:
+        """
+        Invoke the given `fun` for each element in the `self.image`, then return
+        `True`.
+        >>> Iter(1, 3).each(print)
+        1
+        2
+        3
+        """
+        list(map(fun, self.image))
+        return True
+
+    def empty(self) -> bool:
+        """
+        Return `True` if `self.image` is empty, otherwise `False`.
+
+        ```python
+        >>> Iter([]).empty()
+        True
+        >>> Iter(0, 0).empty()
+        True
+        >>> Iter(1, 10).empty()
+        False
+        ```
+        """
+        return not bool(len(self.image))
+
+    def fetch(self, index: int) -> bool:
+        raise NotImplementedError()
+
+    def filter(self, fun: Callable) -> Iter:
+        """
+        Filter `self.image`, i.e. return only those elements for which `fun` returns
+        a truthy value.
+
+        ```python
+        >>> Iter(1, 3).filter(lambda x: x % 2 == 0)
+        [2]
+        ```
+        """
+        self.image = list(filter(fun, self.image))
+        return self
+
+    def find(self, fun: Callable, default: Optional[Any]=None) -> Optional[Any]:
+        """
+        Return the first element for which `fun` returns a truthy value. If no
+        such element is found, return `default`.
+
+        ```python
+        >>> Iter(2, 4).find(lambda x: x % 2 == 1)
+        3
+        >>> Iter([2, 4, 6]).find(lambda x: x % 2 == 1)
+        None
+        >>> Iter([2, 4, 6]).find(lambda x: x % 2 == 1, default=0)
+        0
+        ```
+        """
+        return next(filter(fun, self.image), default)
+
+    def find_index(self, fun: Callable, default: Optional[Any]=None) -> Optional[Any]:
+        """
+        Similar to `self.find`, but return the index (zero-based) of the element
+        instead of the element itself.
+
+        ```python
+        >>> Iter([2, 4, 6]).find_index(lambda x: x % 2 == 1)
+        None
+        >>> Iter([2, 3, 4]).find_index(lambda x: x % 2 == 1)
+        1
+        ```
+        """
+        found = next(filter(fun, self.image), default)
+        return self.image.index(found) if found in self.image else default
+
+    def find_value(self, fun: Callable, default: Optional[Any]=None) -> Optional[Any]:
+        """
+        Similar to `self.find`, but return the value of the function invocation instead
+        of the element itself.
+
+        ```python
+        >>> Iter([2, 4, 6]).find_value(lambda x: x % 2 == 1)
+        None
+        >>> Iter([2, 3, 4]).filter(lambda x: x > 2).find_value(lambda x: x * x)
+        9
+        >>> Iter(1, 3).find_value(lambda x: isinstance(x, bool), "no bools!")
+        'no bools!'
+        ```
+        """
+        found = next(filter(fun, self.image), default)
+        return fun(found) if found is not default else default
+
+    def flat_map(self, fun: Callable) -> Iter:
+        """
+        Map the given `fun` over `self.image` and flattens the result.
+
+        ```python
+        >>> Iter([(1, 3), (4, 6)]).flat_map(lambda x: list(range(x[0], x[1]+1)))
+        [1, 2, 3, 4, 5, 6]
+        >>> Iter([1, 2, 3]).flat_map(lambda x: [[x]])
+        [[1], [2], [3]]
+        ```
+        """
+        self.image = list(itertools.chain(*map(fun, self.image)))
+        return self
+
+    def flat_map_reduce(self, fun: Callable[[Any, Any], Any], acc: Any) -> Iter:
+        """
+        Map and reduce an `self.image`, flattening the given results (only one
+        level deep). It expects an accumulator and a function that receives each
+        enumerable element, and must return a...
+
+        ```python
+        >>> #example
+        ```
+        """
+        raise NotImplementedError()
 
     def __str__(self) -> str:
         return f"[{', '.join(map(str, self.iter))}]" if isinstance(self.image, Iterable) else self.image
--- tests/test_iterfun.py
+++ tests/test_iterfun.py
@@ -1,4 +1,3 @@
-from itertools import accumulate
 import unittest
 import pytest
 
@@ -7,33 +6,38 @@ from src.iterfun import Iter
 class TestIter(unittest.TestCase):
     #region unit tests
 
+    def test_iter(self):
+        self.assertEqual([1, 2, 3], Iter([1, 2, 3]).image)
+        self.assertEqual([1, 2, 3, 4, 5], Iter(1, 5).image)
+        self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Iter(1, 10).image)
+
     def test_all(self):
         self.assertTrue(Iter([1, 2, 3]).all())
         self.assertFalse(Iter([1, None, 3]).all())
         self.assertTrue(Iter([]).all())
-        self.assertTrue(Iter([2,4,6]).all(lambda x: x % 2 == 0))
-        self.assertFalse(Iter([1,2,3]).all(lambda x: x % 2 == 0))
+        self.assertTrue(Iter([2, 4, 6]).all(lambda x: x % 2 == 0))
+        self.assertFalse(Iter([1, 2, 3]).all(lambda x: x % 2 == 0))
         self.assertTrue(Iter([]).all(lambda: None))
 
     def test_any(self):
         self.assertFalse(Iter([False, False, False]).any())
         self.assertTrue(Iter([False, True, False]).any())
         self.assertFalse(Iter([]).any())
-        self.assertFalse(Iter([2,4,6]).any(lambda x: x % 2 == 1))
-        self.assertTrue(Iter([1,2,3]).any(lambda x: x % 2 == 1))
+        self.assertFalse(Iter([2, 4, 6]).any(lambda x: x % 2 == 1))
+        self.assertTrue(Iter([1, 2, 3]).any(lambda x: x % 2 == 1))
         self.assertFalse(Iter([]).any(lambda: None))
 
     def test_at(self):
-        self.assertEqual(2, Iter([2,4,6]).at(0))
-        self.assertEqual(6, Iter([2,4,6]).at(2))
-        self.assertEqual(6, Iter([2,4,6]).at(-1))
+        self.assertEqual(2, Iter([2, 4, 6]).at(0))
+        self.assertEqual(6, Iter([2, 4, 6]).at(2))
+        self.assertEqual(6, Iter([2, 4, 6]).at(-1))
 
     def test_at_throws(self):
         with pytest.raises(IndexError):
-            self.assertEqual(None, Iter([2,4,6]).at(4))
+            self.assertEqual(None, Iter([2, 4, 6]).at(4))
 
     def test_avg(self):
-        self.assertEqual(5, Iter(range(11)).avg())
+        self.assertEqual(5, Iter(0, 10).avg())
 
     def test_chunk_by(self):
         expected = [[1], [2, 2], [3], [4, 4, 6], [7, 7]]
@@ -59,11 +63,85 @@ class TestIter(unittest.TestCase):
         self.assertEqual([1, 2, 3, 4, 5, 6], Iter.concat((1, 3), (4, 6)).image)
 
     def test_count(self):
-        self.assertEqual(3, Iter(range(1, 4)).count())
-        self.assertEqual(2, Iter(range(1, 6)).count(lambda x: x % 2 == 0))
+        self.assertEqual(3, Iter(1, 3).count())
+        self.assertEqual(2, Iter(1, 5).count(lambda x: x % 2 == 0))
+
+    def test_count_until(self):
+        self.assertEqual(5, Iter(1, 20).count_until(5))
+        self.assertEqual(20, Iter(1, 20).count_until(50))
+        self.assertTrue(Iter(1, 10).count_until(10) == 10)
+        self.assertTrue(Iter(1, 12).count_until(10 + 1) > 10)
+        self.assertTrue(Iter(1, 5).count_until(10) < 10)
+        self.assertTrue(Iter(1, 10).count_until(10 + 1) == 10)
+        self.assertEqual(7, Iter(1, 20).count_until(7, lambda x: x % 2 == 0))
+        self.assertTrue(10, Iter(1, 20).count_until(11, lambda x: x % 2 == 0))
+
+    def test_dedup(self):
+        self.assertEqual([1, 2, 3, 2, 1], Iter([1, 2, 3, 3, 2, 1]).dedup().image)
+
+    @pytest.mark.xfail(raises=NotImplementedError, reason="TODO")
+    def test_dedup_by(self):
+        self.assertEqual([5, 1, 3, 2], Iter([5, 1, 2, 3, 2, 1]).dedup_by(lambda x: x > 2).image)
+
+    def test_drop(self):
+        self.assertEqual([3], Iter(1, 3).drop(2).image)
+        self.assertEqual([], Iter(1, 3).drop(10).image)
+        self.assertEqual([1, 2, 3], Iter(1, 3).drop(0).image)
+        self.assertEqual([1, 2], Iter(1, 3).drop(-1).image)
+
+    def test_drop_every(self):
+        self.assertEqual([2, 4, 6, 8, 10], Iter(1, 10).drop_every(2).image)
+        self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Iter(1, 10).drop_every(0).image)
+        self.assertEqual([], Iter(1, 3).drop_every(1).image)
+
+    def test_drop_while(self):
+        self.assertEqual([3, 2, 1], Iter([1, 2, 3, 2, 1]).drop_while(lambda x: x < 3).image)
+
+    def test_each(self):
+        self.assertTrue(Iter(["some", "example"]).each(print))
+
+    def test_empty(self):
+        self.assertTrue(Iter([]).empty())
+        self.assertTrue(Iter(0, 0).empty())
+        self.assertFalse(Iter(1, 10).empty())
+
+    @pytest.mark.xfail(raises=NotImplementedError, reason="TODO")
+    def test_fetch(self):
+        self.assertTrue(Iter([2, 4, 6]).fetch(0).image)
+        self.assertTrue(Iter([2, 4, 6]).fetch(-3).image)
+        self.assertTrue(Iter([2, 4, 6]).fetch(2).image)
+        self.assertFalse(Iter([2, 4, 6]).fetch(4).image)
+
+    def test_filter(self):
+        self.assertEqual([2], Iter(1, 3).filter(lambda x: x % 2 == 0).image)
+
+    def test_find(self):
+        self.assertEqual(3, Iter(2, 4).find(lambda x: x % 2 == 1))
+        self.assertEqual(None, Iter([2, 4, 6]).find(lambda x: x % 2 == 1))
+        self.assertEqual(0, Iter([2, 4, 6]).find(lambda x: x % 2 == 1, default=0))
+
+    def test_find_index(self):
+        self.assertEqual(None, Iter([2, 4, 6]).find_index(lambda x: x % 2 == 1))
+        self.assertEqual(1, Iter([2, 3, 4]).find_index(lambda x: x % 2 == 1))
+
+    def test_find_value(self):
+        self.assertEqual(None, Iter([2, 4, 6]).find_value(lambda x: x % 2 == 1))
+        self.assertTrue(Iter([2, 3, 4]).find_value(lambda x: x % 2 == 1))
+        self.assertEqual(9, Iter([2, 3, 4]).filter(lambda x: x > 2).find_value(lambda x: x * x))
+        self.assertEqual("no bools!", Iter(1, 3).find_value(lambda x: isinstance(x, bool), default="no bools!"))
+
+    def test_flat_map(self):
+        self.assertEqual([1, 2, 3, 4, 5, 6], Iter([(1, 3), (4, 6)]).flat_map(lambda x: list(range(x[0], x[1]+1))).image)
+        self.assertEqual([[1], [2], [3]], Iter([1, 2, 3]).flat_map(lambda x: [[x]]).image)
+
+    @pytest.mark.xfail(raises=NotImplementedError, reason="TODO")
+    def test_flat_map_reduce(self):
+        n = 3
+        fun = lambda x, acc: [[x], acc+1] if acc < n else acc
+        self.assertEqual([[1, 2, 3], 3], Iter(1, 100).flat_map_reduce(fun, acc=0).image)
 
     #endregion
 
-    #region integration tests
+    #region leet code tests
 
     #endregion
